227ef59102cbdfcc91bbb84d35ca82579d621742
Johnny Telles <johnnytelles77@gmail.com>
2024-04-11 08:19:00 -0400

    Segunda entrega

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Para agregar un nuevo producto al sistema, utiliza la función `addProduct`. Est
 Ejemplo:
 
 ```javascript
-addProduct("Producto 1", "Este es un producto de prueba", 19.99, "imagen.jpg", "P002", 50);
+manager.addProduct("Producto 1", "Este es un producto de prueba", 19.99, "imagen.jpg", "P002", 50);
 
 
 ### 2. Ver todos los productos
@@ -28,7 +28,7 @@ Para ver todos los productos existentes en el sistema, utiliza la función getPr
 Ejemplo:
 
 javascript
-getProducts();
+manager.getProducts();
 
 
 ### 3. Buscar un producto por su ID
@@ -38,7 +38,43 @@ id: El ID del producto que deseas buscar.
 Ejemplo:
 
 javascript
-getProductById(2);
+manager.getProductById(2);
 
 
 - Con este código, puedes agregar productos y buscarlos por su identificador (ID). Si intentas agregar un producto que ya existe, recibirás un mensaje en la consola indicando que el producto ya está en existencia. Del mismo modo, si intentas agregar un producto con algún valor faltante (como undefined), recibirás un mensaje en la consola que te indicará que todos los campos son obligatorios
+
+
+### 4. Actualizar un producto
+Para actualizar un producto existente en el sistema, utiliza la función updateProduct. Esta función toma dos argumentos:
+
+id: El ID del producto que deseas actualizar.
+dataProduct: Un objeto que contiene los nuevos datos del producto que deseas actualizar, incluyendo los campos que deseas modificar.
+
+
+Ejemplo:
+
+javascript
+manager.updateProduct(3, {
+    "title": "Nuevo título del producto",
+    "description": "Nueva descripción del producto",
+    "price": 29.99,
+    "thumbnail": "nueva-imagen.jpg",
+    "code": "P003",
+    "stock": 20
+});
+
+
+
+###5. Eliminar un producto
+Para eliminar un producto del sistema por su ID, utiliza la función deleteProduct. Esta función toma un argumento:
+
+id: El ID del producto que deseas eliminar.
+
+
+Ejemplo:
+
+javascript
+manager.deleteProduct(1)
+
+
+Hemos añadido nuevas funciones al programa, como updateProduct y deleteProduct, que mejoran su funcionalidad y lo convierten en un CRUD eficiente. Ahora, además de agregar productos, podemos actualizar y eliminar productos existentes, lo que proporciona una gestión completa de productos. Implementando la capacidad de modificar información en nuestro archivo JSON, lo que añade flexibilidad al sistema.
--- product-manager.js
+++ product-manager.js
@@ -2,14 +2,16 @@ console.log("-----------------")
 console.log("Product Manager")
 console.log("-----------------")
 
+const fs = require("fs")
 
+let pathfile = "./data/products.json"  /// la ruta del json la ponemos en una variable 
 
 class ProductManager {
     constructor() {
         this.products = [];
     }
 
-    addProduct(title, description, price, thumbnail, code, stock) {  // Key Products
+   async addProduct(title, description, price, thumbnail, code, stock) {  // Key Products
         const newProduct = {
             id: this.products.length + 1,
             title,
@@ -32,32 +34,76 @@ class ProductManager {
         }
 
         this.products.push(newProduct);  // agregar un nuevo producto al array 
+        await fs.promises.writeFile(pathfile, JSON.stringify(this.products));
     }
 
-    getProducts() {  // Muestra los productos del array
-        console.log(this.products);
-        return this.products;
+    async getProducts() {  // Muestra los productos del array
+
+        const productsJson = await fs.promises.readFile(pathfile, "utf8")
+        const products = JSON.parse(productsJson) || [];
+/*         console.log(products); */
+
+        return products;
     }
 
-    getProductById(id) {
-        const product = this.products.find(product => product.id === id);  // buscamos en producto en el array en base al Id
+
+
+    async getProductById(id) {  /// buscar los productos por id
+        const products = await this.getProducts(); // Obtener los productos utilizando getProducts()
+        const product = products.find(product => product.id === id); // Buscar el producto en los productos obtenidos
         if (!product) {
-            console.log(`No se ha encontrado el producto con el ID: ${id}`);  // si no lo encuentra, se muestra este mensaje en la consola
+            console.log(`No se ha encontrado el producto con el ID: ${id}`);
             return;
         }
-        console.log(product);
+        console.log("Producto encontrado:", product);
         return product;
     }
+    
+    
+
+    async updateProduct(id, dataProduct) {  /// actualizar los productos
+        const products = await this.getProducts(); // Obtener la lista actualizada de productos
+        const index = products.findIndex(product => product.id === id); // Encontrar el índice del producto con el ID proporcionado
+        if (index === -1) {
+            console.log(`No se encontró ningún producto con el ID: ${id}`);
+            return;
+        }
+        products[index] = {
+            ...products[index],
+            ...dataProduct // Actualizar los datos del producto con los nuevos datos 
+        };
+        await fs.promises.writeFile(pathfile, JSON.stringify(products)); // Escribir los productos actualizados de nuevo en el archivo
+    }
+    
+    async deleteProduct(id) {
+        const products = await this.getProducts(); // Obtener la lista actualizada de productos
+        const updatedProducts = products.filter(product => product.id !== id); // Filtrar los productos para eliminar el producto con el ID proporcionado
+        await fs.promises.writeFile(pathfile, JSON.stringify(updatedProducts)); // Escribir la lista con los cambios actualizados
+    }
+    
 }
+    
+
+
+
 
 // Ejemplo de uso
 const manager = new ProductManager();
-manager.addProduct("Producto 5", "el quinto producto", 899, "http://www.google.com", "ADF126", 10);
+/* manager.addProduct("Producto 5", "el quinto producto", 899, "http://www.google.com", "ADF126", 10);
 manager.addProduct("Producto 1", "el primer producto", 299, "http://www.google.com", "ADF123", 10);
 manager.addProduct("Producto 2", "el segundo producto", 899, "http://www.google.com", "ADF124", 10);
 manager.addProduct("Producto 3", "el tercer producto", 899, "http://www.google.com", "ADF124", 10);
 manager.addProduct("Producto 4", "el cuarto producto", 899, "http://www.google.com", "ADF125", 10);
-manager.addProduct("Producto 6", "el sexto producto", 900, "http://www.google.com", "ADF129", 10);
+manager.addProduct("Producto 6", "el sexto producto", 900, "http://www.google.com", "ADF129", 10);  */
 
-// manager.getProducts();
-manager.getProductById(4);
+
+/* manager.getProducts(); */
+/* manager.getProductById(2); */
+
+/* manager.updateProduct(3, {
+    "title": "Producto 3",
+    "description": "el tercer producto",
+})
+ */
+
+/* manager.deleteProduct(1) */
